4a091c0d14d8e4a2dfc678bdce152226f94ee2c2
Walter Resquin <walter.resquin.py@gmail.com>
2019-09-11 19:12:02 -0300

    aula4 modelos e migracoes

--- paginas/views.py
+++ paginas/views.py
@@ -3,5 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def paginaInicioView(resquest):
-    return HttpResponse('<h1 style="font-size:24px;color:green">AC 02 Finalizada! </br> Bem vindo ao curso de E-commerce!</h1>')
+def paginaInicioView(req):
+    if req.method == 'GET':
+        print(req.GET)
+        return HttpResponse('<h1 style="font-size:24px;color:green">AC 02 Finalizada! </br> Bem vindo ao curso de E-commerce!</h1>')
+
+def paginaAboutView(req):
+    return HttpResponse('<h1> Página com informação sobre o site.</h1>')
+
+def paginaProdutoView(req):
+    return HttpResponse('<h1> Página Produto.</h1>')
+
+def paginaHomeView(req):
+    return render(req, 'paginas/home.html', {})
+    #return HttpResponse('<h1> Página Produto.</h1>')
+
+
